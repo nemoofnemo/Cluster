09bdc25807f355cd5600231ca863d639b40a9d3f
yuanyi <603035634@qq.com>
2017-07-06 13:53:39 +0800

    vld

--- Test/test.cpp
+++ Test/test.cpp
@@ -3,48 +3,20 @@
 #include <list>
 #include <queue>
 
-#define _WIN32_WINNT  0x0501
-#include <boost\asio.hpp>
-#include <boost/thread.hpp>
-#include <boost/bind.hpp>
-#include <boost/function.hpp>
+//#define _WIN32_WINNT  0x0501
+//#include <boost/asio.hpp>
+//#include <boost/thread.hpp>
+//#include <boost/bind.hpp>
+//#include <boost/function.hpp>
 
 using namespace std;
-using namespace boost;
-using namespace boost::asio;
+//using namespace boost;
+//using namespace boost::asio;
+#include "vld.h"
 
-struct kk {
-	int x = 10;
-};
-
-class st {
-public:
-	int a;
-	int b;
-
-	st() {
-		a = 1;
-		b = 2;
-		puts("cccc");
-	}
-
-	~st() {
-		puts("dddd");
-	}
-
-	void show(kk k) {
-		puts("show");
-		cout << k.x << endl;
-	}
-
-	void run() {
-		kk k;
-		bind(&st::show, this, k);
-	}
-};
 
 int main(void) {
-	std::queue<int> q;
-	
+	int * pt = new int;
+	system("pause");
 	return 0;
 }
