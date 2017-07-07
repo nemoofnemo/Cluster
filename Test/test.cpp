5363c3a423f77bb56607c62553fd5de9ebced8ef
yuanyi <603035634@qq.com>
2017-07-07 11:38:13 +0800

    .

--- Test/test.cpp
+++ Test/test.cpp
@@ -24,8 +24,29 @@ public:
 	~A() {
 		cout << "dec" << endl;
 	}
+
+	int operator[](int i) {
+		return 0;
+	}
 };
 
+int binFind(int * a, int length, int val) {
+	int mid = (length - 1) / 2;
+	int start = 0;
+	int end = length - 1;
+	while (end - 1 > start) {
+		if (a[mid] == val) {
+			return mid;
+		}
+		else if (val > a[mid]) {
+			start = mid + 1;
+		}
+		else {
+			end = mid - 1;
+		}
+	}
+	return -1;
+}
 
 int main(void) {
 	char * data = new char[256];
@@ -33,5 +54,7 @@ int main(void) {
 	cout << (void*)data << ' ' << p << ' ' << sizeof(A) << endl;
 	p->~A();
 	delete[] data;
+	p->operator[](1);
+	(*p)[1];
 	return 0;
 }
